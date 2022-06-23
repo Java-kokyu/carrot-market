a43cec5d9922fbc5b11d613c5e449093e0d804d3
Minji Kim <85613861+Java-kokyu@users.noreply.github.com>
2022-06-23 20:58:00 +0900

    페이지네이션

--- src/main/java/com/sparta/clonecoding_8be/repository/PostRepository.java
+++ src/main/java/com/sparta/clonecoding_8be/repository/PostRepository.java
@@ -5,10 +5,18 @@ import com.sparta.clonecoding_8be.model.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+<<<<<<< HEAD
     List<Post> findAllByOrderByCreatedAtDesc();
     Page<Post> findById(Long lastPostId, Pageable pageable);
+=======
+    Page<Post> findByIdLessThanOrderByModifiedAtDesc(Long lastArticleId, Pageable pageable);
+    List<Post> findAllByOrderByModifiedAtDesc();
+//    Page<Post> findById(Long lastPostId, Pageable pageable);
+//    Page<Post> findById(Long lastPostId, Pageable pageable);
+>>>>>>> minji2
 }
